cfc44073c12bdef5aca31007d1af4e2c0240ccfa
Your Name <you@example.com>
2022-06-14 18:34:19 +0300

    remove auth-service and create api-gateway service

---
+++ api-gateway/src/main/java/com/api/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic();
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
--- auth-service/src/main/java/com/authService/model/entity/RoleEnum.java
+++
@@ -1,5 +0,0 @@
-package com.authService.model.entity;
-
-public enum RoleEnum {
-    USER, MODERATOR, ADMIN
-}
